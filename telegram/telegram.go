faaa3355603ad8edf9e202ea57466bf7c27b9aba
Денис Морозов <31116947+Impisigmatus@users.noreply.github.com>
2024-02-28 20:46:34 +0300

    Init

---
+++ telegram/telegram.go
@@ -0,0 +1,68 @@
+package telegram
+
+import (
+	"fmt"
+	"time"
+
+	tg_bot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
+)
+
+type IReciever interface {
+	Handle(payload *tg_bot.Message) (*tg_bot.MessageConfig, error)
+	GetOffset() int
+	GetTimeout() time.Duration
+}
+
+type Telegram struct {
+	api      *tg_bot.BotAPI
+	reciever IReciever
+}
+
+func New(token string, reciever IReciever) *Telegram {
+	if reciever == nil {
+		logrus.Panicf("Invalid init: reciever is nil")
+	}
+	api, err := tg_bot.NewBotAPI(token)
+	if err != nil {
+		logrus.Panicf("Invalid telegram api: %s", err)
+	}
+	return newTelegram(api, reciever)
+}
+
+func newTelegram(api *tg_bot.BotAPI, reciever IReciever) *Telegram {
+	tg := Telegram{api: api, reciever: reciever}
+	tg.consume()
+	return &tg
+}
+
+func (tg *Telegram) Send(chatID uint64, data string) error {
+	msg := tg_bot.NewMessage(int64(chatID), data)
+	if _, err := tg.api.Send(msg); err != nil {
+		return fmt.Errorf("Invalid send: %s", err)
+	}
+
+	return nil
+}
+
+func (tg *Telegram) consume() {
+	go func() {
+		updater := tg_bot.NewUpdate(tg.reciever.GetOffset())
+		updater.Timeout = int(tg.reciever.GetTimeout().Seconds())
+		updates := tg.api.GetUpdatesChan(updater)
+
+		for update := range updates {
+			if update.Message != nil {
+				msg, err := tg.reciever.Handle(update.Message)
+				if err != nil {
+					logrus.Errorf("Invalid handle msg: %s", err)
+					continue
+				}
+				if _, err := tg.api.Send(msg); err != nil {
+					logrus.Errorf("Invalid send: %s", err)
+					continue
+				}
+			}
+		}
+	}()
+}
